c5695907280f713a8b351607c593c5b88cd6ebdc
Lucid <lpprojecttt06@gmail.com>
2025-08-30 15:04:10 +0300

    Update InputUtils.lua

--- Client/Library/Utils/InputUtils.lua
+++ Client/Library/Utils/InputUtils.lua
@@ -1,1 +1,9 @@
+-- InputUtils.lua
+local UserInputService = game:GetService("UserInputService")
+local InputUtils = {}
 
+function InputUtils:IsKeyDown(key)
+    return UserInputService:IsKeyDown(Enum.KeyCode[key])
+end
+
+return InputUtils
