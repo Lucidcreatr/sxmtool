cd48e48e913ac1d386f3de71cbebbbf4aa6e6078
Lucid <lpprojecttt06@gmail.com>
2025-08-30 14:59:16 +0300

    Update Core.lua

--- Client/Library/Core.lua
+++ Client/Library/Core.lua
@@ -1,1 +1,21 @@
+-- Core.lua
+local Window = require(script:WaitForChild("Components"):WaitForChild("Window"))
+local Section = require(script:WaitForChild("Components"):WaitForChild("Section"))
+
+local Core = {}
+Core.__index = Core
+
+function Core.new()
+    local self = setmetatable({}, Core)
+    self.Windows = {}
+    return self
+end
+
+function Core:CreateWindow(opts, theme)
+    local win = Window.new(opts, theme)
+    table.insert(self.Windows, win)
+    return win
+end
+
+return Core
 
