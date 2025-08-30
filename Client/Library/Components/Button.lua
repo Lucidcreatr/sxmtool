9fd4e54bff22a07d4bba399f4c35a979de1b1367
Lucid <lpprojecttt06@gmail.com>
2025-08-30 15:03:09 +0300

    Update Button.lua

--- Client/Library/Components/Button.lua
+++ Client/Library/Components/Button.lua
@@ -1,1 +1,15 @@
+-- Button.lua
+local Button = {}
+Button.__index = Button
 
+function Button.new(parent, opts)
+    local btn = Instance.new("TextButton", parent)
+    btn.Size = UDim2.new(0,150,0,28)
+    btn.Text = opts.label or "Button"
+    btn.MouseButton1Click:Connect(function()
+        if opts.callback then opts.callback() end
+    end)
+    return btn
+end
+
+return Button
